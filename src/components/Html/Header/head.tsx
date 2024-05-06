98618a3f7d6506cfdeff7b19cec5b544fe768a3b
taynanhott <45300380+taynanhott@users.noreply.github.com>
2024-05-06 17:25:17 -0300

    fix layout theme

--- src/components/Html/Header/head.tsx
+++ src/components/Html/Header/head.tsx
@@ -3,19 +3,17 @@ import { NavigationMenuDemo } from "../../Resources/NavigationMenu/navigation";
 
 export function Head() {
     return (
-        <div className="z-20 relative border backdrop-blur-sm flex items-center justify-between py-4 mb-10">
-            <div>
-                <div className="flex items-center">
-                    <a
-                        href="https://portfolio-taynanzh.vercel.app/home"
-                        target="_self"
-                    >
-                        <p className="font-bold mr-4 text-xl lg:text-2xl ml-4">Portfólio</p>
-                    </a>
-                    <NavigationMenuDemo />
-                </div>
+        <div className="z-20 relative border shadow-lg backdrop-blur-sm flex items-center justify-between py-4 mb-10">
+            <div className="flex items-center">
+                <a
+                    href="https://portfolio-taynanzh.vercel.app/home"
+                    target="_self"
+                >
+                    <p className="font-bold mr-4 text-xl lg:text-2xl ml-4">Portfólio</p>
+                </a>
+                <NavigationMenuDemo />
             </div>
-            <div className="flex items-center mr-10">
+            <div className="flex mr-4 items-center">
                 <ModeToggle />
             </div>
         </div>
--- src/components/Resources/ModeToggle/index.tsx
+++ src/components/Resources/ModeToggle/index.tsx
@@ -1,37 +1,42 @@
 "use client"
 
 import * as React from "react"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "@/components/ui/navigation-menu"
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 
 export function ModeToggle() {
     const { setTheme } = useTheme()
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Tema</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Claro
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Escuro
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <NavigationMenu className="cursor-pointer">
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>
+                        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] mr-2 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Tema</span>
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="gap-3 p-2 lg:grid-cols-[.75fr_1fr]">
+                            <li onClick={() => setTheme("light")} className="p-2 mb-2 rounded-md hover:bg-gray-600 hover:text-white">
+                                Claro
+                            </li>
+                            <li onClick={() => setTheme("dark")} className="p-2 rounded-md hover:bg-gray-600 hover:text-white">
+                                Escuro
+                            </li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     )
 }
