3be4fa32f69956ec1a542bf6a12d8f20f9dae439
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-05-10 23:06:50 -0300

    fix type

--- src/components/use-dimensions.tsx
+++ src/components/use-dimensions.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from "react";
 
-export const useDimensions = ref => {
+export const useDimensions = (ref: React.RefObject<HTMLElement>) => {
   const dimensions = useRef({ width: 0, height: 0 });
 
   useEffect(() => {
     dimensions.current.width = ref.current.offsetWidth;
     dimensions.current.height = ref.current.offsetHeight;
-  }, []);
+  }, [ref]);
 
   return dimensions.current;
 };
